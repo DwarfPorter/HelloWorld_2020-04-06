7d80bde24d08f60eabdc8cfc7658947b2b8115f8
DwarfPorter <mrvlmor@gmail.com>
2020-04-06 21:56:18 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = findViewById(R.id.text1);
+        String txt = text.getText().toString();
+        text.setText(txt + "!!");
+    }
+}
