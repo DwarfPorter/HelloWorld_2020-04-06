6563bf1c78d874645336e34ddb04097dc3f678dd
DwarfPorter <mrvlmor@gmail.com>
2020-04-06 22:11:08 +0300

    Обработка нажатий на кнопку

--- app/src/main/java/ru/geekbrains/MainActivity.java
+++ app/src/main/java/ru/geekbrains/MainActivity.java
@@ -2,6 +2,7 @@ package ru.geekbrains;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text = findViewById(R.id.text1);
+        final TextView text = findViewById(R.id.text1);
         String txt = text.getText().toString();
         text.setText(txt + "!!");
 
         Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("Нажали");
+            }
+        });
     }
 }
